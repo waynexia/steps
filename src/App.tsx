23f25311cb19292b799dfad5a3095f8329e68b9e
Ruihang Xia <waynestxia@gmail.com>
2024-08-04 17:44:15 +0800

    feat: handle scroll behavior on timeline
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { mock_list } from './list'
 import { wikipedia_link_of_page, wikipedia_link_of_year } from './utils'
 
 function App() {
   const [list, setList] = useState<{ from: number, to: number, person: { desc: string, link: string | undefined, death: number | undefined }, other_people: { desc: string, link: string | undefined, death: number | undefined }[] }[]>([])
 
+  // const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentTimelineHighlight, setCurrentTimelineHighlight] = useState(0)
+
+  // prepare (mock) data
   useEffect(() => {
     const fetchData = async () => {
       // const data = await build_list(100, 800)
@@ -15,41 +19,39 @@ function App() {
     fetchData()
   }, [])
 
+  const handleScrollDebug: React.UIEventHandler<HTMLDivElement> = (e) => {
+    const { scrollTop, scrollHeight, clientHeight } = e.target as HTMLDivElement
+    const position = Math.ceil(
+      (scrollTop / (scrollHeight - clientHeight)) * 1000,
+    )
+    setCurrentTimelineHighlight(position)
+  }
+
   return (
-    <div className="container">
-      <div className="scroll-side">
-        <ul>
-          <table>
-            {list.map((item, _index) => (
-              <tr key={item.from}>
-                <th>
-                  <a href={wikipedia_link_of_year(item.from)}>{item.from}</a>
-                </th>
-                <th>
-                  {item.person.link !== undefined && (
-                    <a href={wikipedia_link_of_page(item.person.link)}>{item.person.desc}</a>
-                  )}
-                </th>
+    <div className="gallery">
+      <div className="progress-bar" onScroll={handleScrollDebug}>
+        vertical timeline
+        <table>
+          <tbody>
+            {Array.from({ length: 1000 }).map((_, index) => (
+              <tr key={index}>
+                <td>
+                  Row
+                  {index === currentTimelineHighlight ? 99999 : index}
+                </td>
               </tr>
             ))}
-          </table>
-        </ul>
+          </tbody>
+        </table>
       </div>
 
-      <div className="header">
-
+      <div className="text-detail">
+        {/* <h1>{list[currentIndex].person.desc}</h1> */}
+        <p>description</p>
       </div>
 
-      <div className="main">
-
-      </div>
-
-      <div className="aside">
-
-      </div>
-
-      <div className="footer">
-
+      <div className="photo">
+        some image
       </div>
     </div>
 
